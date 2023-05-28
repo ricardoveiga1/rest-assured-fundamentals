e09fb360a7bc8b3d13295f70ba8a03b9d7b752ac
ricardoveiga1 <ricardoveiga.ti@gmail.com>
2023-05-28 16:20:30 -0300

    classes

--- src/test/java/config/FootballTest.java
+++ src/test/java/config/FootballTest.java
@@ -1,6 +1,10 @@
 package config;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -42,5 +46,62 @@ public class FootballTest extends FootballConfig{
                 .body("teams[0].name", equalTo("Arsenal FC"))
                 .body("teams[1].name", equalTo("Aston Villa FC"));
     }
+
+    @Test
+    public void getAllTeamData(){
+        String responseBody = get("teams/57").asString();
+        System.out.println(responseBody);
+    }
+
+    @Test
+    public void getAllTeamData_DoCheckirst(){
+        Response response =
+                given()
+                .when()
+                        .get("teams/57")
+                .then()
+                        .contentType(ContentType.JSON)
+                        .extract().response();
+
+        String jsonResponseAsString = response.asString();
+        System.out.println(jsonResponseAsString);
+    }
+
+    @Test
+    public void extractHeaders(){
+        Response response =
+                get("teams/57")
+                .then()
+                        .contentType(ContentType.JSON)
+                        .extract().response();
+
+        String contentTypeHeader = response.getContentType();
+        System.out.println("Content Type: " + contentTypeHeader);
+
+        String apiVersionHeader = response.getHeader("X-API-Version");
+        System.out.println("Content Type: " + apiVersionHeader);
+    }
+
+    @Test
+    public void extractFirstTeamName(){
+        String firstTeam = get("/competitions/2021/teams").jsonPath().getString("teams.name[0]");
+        System.out.println(firstTeam);
+    }
+
+    @Test
+    public void extractAllTeamNames(){
+        Response response = get("/competitions/2021/teams")
+                .then()
+                        .extract().response();
+
+        List<String> teamNames = response.path("teams.name");
+
+        for(String teamName : teamNames){
+            System.out.println(teamName);
+        }
+
+    }
+
+
 
 }
